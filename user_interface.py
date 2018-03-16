6fc4a5c2c191ce613b25ea4d6e0b9eb7b0e783c6
bliutwo <bliutwo@cs.stanford.edu>
2018-03-16 08:34:54 -0700

    Finish functions process_disease_file, all_symptoms; Update format of polished_diseases

--- user_interface.py
+++ user_interface.py
@@ -8,6 +8,44 @@ import time
 
 def process_disease_file(filename):
     d = {}
+    with open(filename) as f:
+        content= f.readlines()
+    content = [x.strip() for x in content]
+   
+    # empty list containing two empty lists
+    lol = []
+    lol.append([])
+    lol.append([])
+ 
+    counter = 0
+    curr_key = ''
+
+    for line in content:
+        if counter == 0:
+            curr_key = line
+            d[line] = lol
+            counter += 1
+        elif counter == 1:
+            # do nothing to d, because line says symptom
+            # add one to counter, should be 2 now
+            counter +=1
+        elif line == ';':
+            # do nothing to d, because next line begins treatments
+            # add one to counter, should be 3 now
+            counter += 1
+        elif line == ':':
+            # end of treatments, the next one will be a disease
+            counter = 0
+        elif counter == 2:
+            # we are adding symptoms
+            d[curr_key][0].append(line)
+        elif counter == 3:
+            # we are adding treatments
+            d[curr_key][1].append(line)
+        else:
+            print "Error, should not be able to get here"
+            assert(False)
+            
     return d
 
 def get_diseases(user_input, disease_ontology):
@@ -17,12 +55,25 @@ def get_diseases(user_input, disease_ontology):
 def all_symptoms(disease_ontology):
     string = ""
 
-    for symptom in disease_ontology:
-        string.append(symptom)
-        string.append(", ")
-        # TODO: how do i know what the last element of the list is?
-        #       i don't want a comma after that one
-    
+    diseases = disease_ontology.keys()
+
+    counter = 0
+
+    # calculate limit
+    # how i know what the last element of the list is
+    # i don't want a comma after that one
+    limit = 0
+    for disease in diseases:
+        for symptom in disease_ontology[disease][0]:
+            limit += 1
+
+    for disease in diseases:
+        for symptom in disease_ontology[disease][0]:
+            string += symptom
+            counter += 1
+            if counter != limit:
+                string += ', '
+                
     return string
 
 
@@ -30,11 +81,13 @@ def main():
     print "Calculating..."
     start = time.time()
 
-    filename = "diseases.txt"
+    filename = "polished_diseases.txt"
 
     disease_ontology = process_disease_file(filename)
+    print disease_ontology
 
     print all_symptoms(disease_ontology)
+    assert(False)
 
     prompt = "Input a list of symptoms separated by a comma (type 'e' to exit): "
     
